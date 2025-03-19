a59892828a0dd422c8d6f314e2478b47a32ded18
Gias <khaitov.gias@gmail.com>
2025-03-19 20:16:02 +0500

    [Feature] Added MainTabView

---
+++ WhatsApp-clone/Screens/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 19/03/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
