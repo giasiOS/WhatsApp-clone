f9ea25b17209dc588f3f144eff5989087384d739
Gias <khaitov.gias@gmail.com>
2025-03-19 19:51:44 +0500

    Initial Commit

---
+++ WhatsApp-clone/WhatsApp_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_cloneApp.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
