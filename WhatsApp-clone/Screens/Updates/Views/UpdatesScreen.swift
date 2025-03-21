a3281a234779c80e28e2c158acc4955a19444733
Gias <khaitov.gias@gmail.com>
2025-03-21 10:50:44 +0500

    [Feature] Added UpdatesSreen into Uptadets group inside Views folder

---
+++ WhatsApp-clone/Screens/Updates/Views/UpdatesScreen.swift
@@ -0,0 +1,18 @@
+//
+//  UpdatesScreen.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 21/03/2025.
+//
+
+import SwiftUI
+
+struct UpdatesScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdatesScreen()
+}
