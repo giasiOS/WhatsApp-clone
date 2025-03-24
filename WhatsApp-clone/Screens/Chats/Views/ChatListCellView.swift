4861da6b90975a15c9a88c34e7f66afced64aa0d
Gias <khaitov.gias@gmail.com>
2025-03-24 19:57:00 +0500

    [Feature] Added empty ChatListCellView

---
+++ WhatsApp-clone/Screens/Chats/Views/ChatListCellView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListCellView.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 24/03/2025.
+//
+
+import SwiftUI
+
+struct ChatListCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatListCellView()
+}
