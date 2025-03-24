32525ce6fec4ba45e0985d595db2e8051930205a
Gias <khaitov.gias@gmail.com>
2025-03-24 19:08:34 +0500

    [Feature] Added empty ChatsScreen.swift in Screens/Chats/Views

---
+++ WhatsApp-clone/Screens/Chats/Views/ChatsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ChatsScreen.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 24/03/2025.
+//
+
+import SwiftUI
+
+struct ChatsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatsScreen()
+}
