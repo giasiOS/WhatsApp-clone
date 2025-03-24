15ba294ab4f93e9c3c79e02841cdc82e074ca64a
Gias <khaitov.gias@gmail.com>
2025-03-24 19:42:24 +0500

    [Feature] feat: Added ChatsScreen with search and toolbar buttons
    - Implemented `ChatsScreen` using `NavigationStack`
    - Added an empty `List` for future chat items
    - Integrated search functionality with `.searchable(text:)`
    - Added toolbar buttons: \"More Options\" (leading), \"New Chat\", \"Camera\", and \"Status\" (trailing)
    - Applied `.toolbar` for structured navigation controls

--- WhatsApp-clone/Screens/Chats/Views/ChatsScreen.swift
+++ WhatsApp-clone/Screens/Chats/Views/ChatsScreen.swift
@@ -9,10 +9,62 @@ import SwiftUI
 
 struct ChatsScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                
+            }
+            .searchable(text: .constant(""))
+            .navigationTitle("Chats")
+            .toolbar {
+                trailingNavItems
+            }
+        }
     }
 }
 
 #Preview {
     ChatsScreen()
+}
+
+extension ChatsScreen {
+    
+    @ToolbarContentBuilder
+    private var trailingNavItems: some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+                
+            } label: {
+                Image(systemName: "circle")
+                    .bold()
+                    .imageScale(.medium)
+                    .foregroundStyle(LinearGradient(colors: [.teal, .purple], startPoint: .leading, endPoint: .bottomTrailing))
+            }
+        }
+        
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+                
+            } label: {
+                Image(systemName: "camera")
+                  
+            }
+        }
+        
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+                
+            } label: {
+                Image(systemName: "plus.circle.fill")
+                    .foregroundStyle(.whatsAppWhite, .blue)
+            }
+        }
+        
+        ToolbarItem(placement: .topBarLeading) {
+            Button {
+                
+            } label: {
+                Image(systemName: "ellipsis.circle")
+            }
+        }
+    }
 }
