730d3e6603404840c87fb7ca6134a34b3b01f836
Gias <khaitov.gias@gmail.com>
2025-03-24 17:54:10 +0500

    [Feature]  Added CommunitiesScreen with UI components and navigation
    - Implemented `CommunitiesScreen` using `NavigationStack`
    - Added placeholder image and text sections
    - Integrated buttons for "See Example Communities" and "New Community"
    - Applied `.scrollIndicators(.hidden)` for a cleaner UI

--- WhatsApp-clone/Screens/Communities/Views/CommunitiesScreen.swift
+++ WhatsApp-clone/Screens/Communities/Views/CommunitiesScreen.swift
@@ -9,10 +9,68 @@ import SwiftUI
 
 struct CommunitiesScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 16) {
+                    PlacehoderImageView
+                    PlaceholderTextSectionView
+                    SeeExampleCommunitiesButton
+                    NewCommunityAddButton
+                }
+                .padding()
+            }.scrollIndicators(.hidden)
+                .navigationTitle("Communities")
+        }
     }
 }
 
 #Preview {
     CommunitiesScreen()
+}
+
+extension CommunitiesScreen {
+    
+    private var PlacehoderImageView: some View {
+        Image(.communities)
+            .resizable()
+            .scaledToFit()
+            .frame(maxWidth: .infinity)
+            .frame(height: 200)
+            .padding(.top)
+    }
+    
+    private var PlaceholderTextSectionView: some View {
+        VStack(alignment:.leading, spacing: 16) {
+            Text("Stay connected with a community")
+                .font(.headline)
+            
+            Text("Communities bring members together in topic-based groups. Any community you're added to will appear here.")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+        }
+    }
+    
+    private var SeeExampleCommunitiesButton: some View {
+        Button("See exmaple communities>") {
+            
+        }
+        .font(.headline)
+    }
+    
+    private var NewCommunityAddButton: some View {
+        Button {
+            
+        } label: {
+            Label("New Community", systemImage: "plus")
+                .font(.subheadline).bold()
+                .foregroundStyle(.whatsAppWhite)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical,10)
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 14))
+                .padding(.horizontal)
+                .padding(.top,10)
+                
+        }
+    }
 }
