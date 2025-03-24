b17423442c68fc1f1b88b5e9d92aa06b35d1d6db
Gias <khaitov.gias@gmail.com>
2025-03-24 17:38:58 +0500

    [Feature] Added empty CommunitiesScreen.swift in Screens/Communities/Views

---
+++ WhatsApp-clone/Screens/Communities/Views/CommunitiesScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CommunitiesScreen.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 24/03/2025.
+//
+
+import SwiftUI
+
+struct CommunitiesScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunitiesScreen()
+}
