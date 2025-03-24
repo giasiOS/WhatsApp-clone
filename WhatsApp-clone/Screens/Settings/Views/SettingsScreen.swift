b5b2532d09bcac3206549f94b39f09d69dc72f28
Gias <khaitov.gias@gmail.com>
2025-03-24 21:00:06 +0500

    [Feature] Added empty SettingsScreen.swift in Screens/Settings/Views

---
+++ WhatsApp-clone/Screens/Settings/Views/SettingsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsScreen.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 24/03/2025.
+//
+
+import SwiftUI
+
+struct SettingsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsScreen()
+}
