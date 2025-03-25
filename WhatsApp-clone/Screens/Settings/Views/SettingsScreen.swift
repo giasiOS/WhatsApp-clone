553810046fff2f287e1193af2748322cac5d494c
Gias <khaitov.gias@gmail.com>
2025-03-25 11:52:45 +0500

    [Feature] Added SettingsScreen with basic layout and navigation

--- WhatsApp-clone/Screens/Settings/Views/SettingsScreen.swift
+++ WhatsApp-clone/Screens/Settings/Views/SettingsScreen.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct SettingsScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                
+            }
+            .searchable(text: .constant(""))
+            .navigationTitle("Settings")
+        }
     }
 }
 
