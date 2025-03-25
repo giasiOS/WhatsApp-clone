b01c04fcad69f838e968cde4063b75f500dba470
Gias <khaitov.gias@gmail.com>
2025-03-25 12:07:03 +0500

    [Feature] Added empty SettingsListCellView as a placeholder

---
+++ WhatsApp-clone/Screens/Settings/Views/SettingsListCellView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsListCellView.swift
+//  WhatsApp-clone
+//
+//  Created by ali on 25/03/2025.
+//
+
+import SwiftUI
+
+struct SettingsListCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsListCellView()
+}
