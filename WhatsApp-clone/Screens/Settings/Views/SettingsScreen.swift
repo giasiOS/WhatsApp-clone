f672346261bef55e8eae7f118f7d8c6410b714b4
Gias <khaitov.gias@gmail.com>
2025-03-25 13:31:36 +0500

    [Feature] Implemented SettingsScreen with structured sections
    - Added ProfileCellView for user profile display
    - Integrated SettingsListCellView for categorized settings options
    - Organized sections for better navigation experience

--- WhatsApp-clone/Screens/Settings/Views/SettingsScreen.swift
+++ WhatsApp-clone/Screens/Settings/Views/SettingsScreen.swift
@@ -11,7 +11,30 @@ struct SettingsScreen: View {
     var body: some View {
         NavigationStack {
             List {
+                // Header
+                Section {
+                    ProfileCellView()
+                    SettingsListCellView(item: .media)
+                }
+                // Body
+                Section {
+                    SettingsListCellView(item: .broadCastLists)
+                    SettingsListCellView(item: .starredMessages)
+                    SettingsListCellView(item: .linkedDevices)
+                }
                 
+                Section {
+                    SettingsListCellView(item: .account)
+                    SettingsListCellView(item: .privacy)
+                    SettingsListCellView(item: .chats)
+                    SettingsListCellView(item: .notfications)
+                    SettingsListCellView(item: .storage)
+                }
+                
+                Section {
+                    SettingsListCellView(item: .help)
+                    SettingsListCellView(item: .tellFriend)
+                }
             }
             .searchable(text: .constant(""))
             .navigationTitle("Settings")
@@ -21,4 +44,37 @@ struct SettingsScreen: View {
 
 #Preview {
     SettingsScreen()
+}
+
+extension SettingsScreen {
+    
+    private struct ProfileCellView: View {
+        var body: some View {
+            HStack {
+                Circle()
+                    .frame(width: 48, height: 48)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Evgeny")
+                            .font(.title3)
+                            .fontWeight(.medium)
+                            
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "qrcode")
+                                .padding(6)
+                                .foregroundStyle(.blue)
+                                .background(Color(.systemGroupedBackground))
+                                .clipShape(Circle())
+                        }
+                        .frame(maxWidth: .infinity,alignment: .trailing)
+                    }
+                    Text("Hey there! I am using WhatsApp.")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                }
+            }
+        }
+    }
 }
